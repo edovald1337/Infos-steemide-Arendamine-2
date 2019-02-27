984b17160b32c3be3ea3c21143a9d1a0b58226bc
Raul Madis Edovald <raedov@ttu.ee>
2019-02-27 23:51:14 +0200

    c

---
+++ Week04/WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        public string GetString()
+        {
+            return "Hello world!";
+        }
+
+        public ActionResult GetView()
+        {
+            Employee emp = new Employee();
+            emp.FirstName = "Sukesh";
+            emp.LastName = "Marla";
+            emp.Salary = 20000;
+        }
+    }
+}
